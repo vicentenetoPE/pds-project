e9861c717a26d39937cf13d2195330187cb6378b
vicente <vicentenetope@hotmail.com>
2024-02-19 15:43:16 -0300

    inseri os primeiros arquivos de autenticação

--- gps-back/src/app.module.ts
+++ gps-back/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './api/auth/auth.module';
+import { UsersModule } from './api/users/users.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    AuthModule,
+    UsersModule,
+  ]
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- gps-back/src/main.ts
+++ gps-back/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const env = app.get(ConfigService);
+  const SERVER_PORT = env.get<string>('APP_PORT') ?? '3003';
+
+
+  await app.listen(SERVER_PORT, '0.0.0.0');
+  const address = await app.getUrl();
+  console.log('Server is running on: ', address);
 }
 bootstrap();
