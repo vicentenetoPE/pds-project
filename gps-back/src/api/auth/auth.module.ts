7b89a3d0126b0483692558048f0019ded518410d
vicentenetoPE <90466583+vicentenetoPE@users.noreply.github.com>
2024-03-11 00:57:56 -0300

    adicionado o pacote do jwt

--- gps-back/src/api/auth/auth.module.ts
+++ gps-back/src/api/auth/auth.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
-import {JwtModule} from '@nestjs/jwt'
+import { JwtModule } from '@nestjs/jwt'
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 
